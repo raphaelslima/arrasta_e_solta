962706fb55afbbd26156f546ec957fadbd35da94
Raphael Lima <raphaeldesousalm@gmail.com>
2021-09-09 17:58:46 -0300

    Projeto Final

---
+++ script.js
@@ -0,0 +1,97 @@
+// Data Initial
+
+let areas = {
+  a: null,
+  b: null,
+  c: null
+}
+
+// Event
+
+document.querySelectorAll('.item').forEach(item => {
+  item.addEventListener('dragstart', dragStart)
+  item.addEventListener('dragend', dragEnd)
+})
+
+document.querySelectorAll('.area').forEach(area => {
+  area.addEventListener('dragover', dragOver)
+  area.addEventListener('dragleave', dragLeave)
+  area.addEventListener('drop', drop)
+})
+
+document
+  .querySelector('.neutralArea')
+  .addEventListener('dragover', dragOverNeutral)
+document
+  .querySelector('.neutralArea')
+  .addEventListener('dragleave', dragLeaveNeutral)
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral)
+
+// Funcion item
+function dragStart(e) {
+  e.currentTarget.classList.add('dragging')
+}
+
+function dragEnd(e) {
+  e.currentTarget.classList.remove('dragging')
+}
+
+// function Área
+
+function dragOver(e) {
+  if (e.currentTarget.querySelector('.item') === null) {
+    e.preventDefault() // Precisa habilitar pro drop funcionar
+    e.currentTarget.classList.add('hover')
+  }
+}
+
+function dragLeave(e) {
+  e.currentTarget.classList.remove('hover')
+}
+
+function drop(e) {
+  e.currentTarget.classList.remove('hover')
+
+  if (e.currentTarget.querySelector('.item') === null) {
+    let dragItem = document.querySelector('.item.dragging')
+    e.currentTarget.appendChild(dragItem)
+    updateLogic()
+  }
+}
+
+//Function NeutralArea
+
+function dragOverNeutral(e) {
+  e.preventDefault()
+  e.currentTarget.classList.add('hover')
+}
+
+function dragLeaveNeutral(e) {
+  e.currentTarget.classList.remove('hover')
+}
+
+function dropNeutral(e) {
+  e.currentTarget.classList.remove('hover')
+  let dragItem = document.querySelector('.item.dragging')
+  e.currentTarget.appendChild(dragItem)
+  updateLogic()
+}
+
+// logic functon
+
+function updateLogic() {
+  document.querySelectorAll('.area').forEach(area => {
+    let name = area.getAttribute('data-name')
+
+    if (area.querySelector('.item') !== null) {
+      areas[name] = area.querySelector('.item').innerHTML
+    } else {
+      areas[name] = null
+    }
+  })
+  if (areas.a === '1' && areas.b === '2' && areas.c === '3') {
+    document.querySelector('.areas').classList.add('correct')
+  } else {
+    document.querySelector('.areas').classList.remove('correct')
+  }
+}
